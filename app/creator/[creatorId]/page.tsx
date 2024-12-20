53c1a92cfce9f3b4b0b7acd3837b3dfec13430af
Omkara07 <singh.omkara07@gmail.com>
2024-12-21 00:34:50 +0530

    creator_page-fix

--- app/creator/[creatorId]/page.tsx
+++ app/creator/[creatorId]/page.tsx
@@ -2,14 +2,20 @@ import Redirect from '@/components/Redirect'
 import UserDashboardPage from '@/components/ui/userDashboardPage'
 import React from 'react'
 
-const page = ({ params }: { params: { creatorId: string } }) => {
-    const { creatorId } = params
+interface PageProps {
+    params: {
+        creatorId: string;
+    };
+}
+
+const Page: React.FC<PageProps> = ({ params }) => {
+    const { creatorId } = params;
     return (
         <>
             <Redirect />
             <UserDashboardPage creatorId={creatorId} canPlay={false} />
         </>
-    )
+    );
 }
 
-export default page
+export default Page;
